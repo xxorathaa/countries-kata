e8f6b4cbd185e26e67ed9effab7f0034649ea16c
mhy <xxorathaa@gmail.com>
2025-06-14 02:21:04 -0500

    basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import './App.css';
 import CountryList from './components/CountryList';
+import {
+  BrowserRouter,
+  Route,
+  Routes
+} from "react-router-dom";
 
 function App() {
 
   return (
     <div className="App">
-      <CountryList />
+      <BrowserRouter>
+        <Routes>
+          <Route path="list" Component={CountryList} />
+          <Route path="details" Component={CountryList} />
+          <Route path=""  Component={CountryList} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
